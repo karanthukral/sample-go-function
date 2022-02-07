2f7d3ad88fd9f714ec318ecee68e71342608c1b1
Karan Thukral <karanthukral@gmail.com>
2022-02-07 12:16:38 -0500

    update connection string

--- packages/default/hello/hello.go
+++ packages/default/hello/hello.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/xo/dburl"
 	"os"
 )
 
@@ -16,9 +17,16 @@ func Main(args map[string]interface{}) map[string]interface{} {
 	msg["body"] = "Hello " + name + "!" + os.Getenv("DATABASE_URL")
 
 	// Open up our database connection.
-	// I've set up a database on my local machine using phpmyadmin.
-	// The database is called testDb
-	db, err := sql.Open("mysql", os.Getenv("DATABASE_URL"))
+	dbURL, err := dburl.Parse(os.Getenv("DATABASE_URL"))
+	if err != nil {
+		panic(err.Error())
+	}
+
+	dbPassword, _ := dbURL.User.Password()
+	dbName := strings.Trim(dbURL.Path, "/")
+	connectionString := fmt.Sprintf("%s:%s@(%s:%s)/%s?charset=utf8&parseTime=true", dbURL.User.Username(), dbPassword, dbURL.Hostname(), dbURL.Port(), dbName)
+
+	db, err := sql.Open("mysql", connectionString)
 	if err != nil {
 		panic(err.Error())
 	}
