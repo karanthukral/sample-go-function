f75afc873644e1e92a8c26289d186b89335438b2
Karan Thukral <karanthukral@gmail.com>
2022-02-07 10:59:06 -0500

    restart

--- db-function/packages/default/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/karanthukral/sample-go-function/db-function/packages/default
-
-go 1.17
-
-require github.com/go-sql-driver/mysql v1.6.0
--- packages/default/hello.go
+++ packages/default/hello.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	"os"
-
 	_ "github.com/go-sql-driver/mysql"
+	"os"
 )
 
 func Main(args map[string]interface{}) map[string]interface{} {
@@ -14,12 +11,12 @@ func Main(args map[string]interface{}) map[string]interface{} {
 		name = "stranger"
 	}
 	msg := make(map[string]interface{})
-	msg["body"] = "Hello " + name + "!"
+	msg["body"] = "Hello " + name + "!" + os.Getenv("DATABASE_URL")
 
 	// Open up our database connection.
 	// I've set up a database on my local machine using phpmyadmin.
 	// The database is called testDb
-	db, err := sql.Open("mysql", os.Getenv("DB_URL"))
+	db, err := sql.Open("mysql", os.Getenv("DATABASE_URL"))
 	if err != nil {
 		panic(err.Error())
 	}
