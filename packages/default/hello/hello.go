acedb0922fbddb9323885c0f51586fb8cb927e33
Karan Thukral <karanthukral@gmail.com>
2022-03-29 12:51:03 -0400

    just pring url

--- packages/default/hello/hello.go
+++ packages/default/hello/hello.go
@@ -1,13 +1,8 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"os"
-
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 func Main(args map[string]interface{}) map[string]interface{} {
@@ -16,18 +11,18 @@ func Main(args map[string]interface{}) map[string]interface{} {
 		name = "stranger"
 	}
 	msg := make(map[string]interface{})
-	msg["body"] = "Hello " + name + "!"
+	msg["body"] = "Hello " + name + "!" + os.Getenv("DB_URL")
 
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(os.Getenv("DB_URL")))
-	if err != nil {
-		fmt.Printf("errored connecting to mongo: %s", err.Error())
-		// panic(err.Error())
-	} else {
-		if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
-			fmt.Printf("errored pinging mongo: %s", err.Error())
-			// panic(err)
-		}
-	}
+	// client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(os.Getenv("DB_URL")))
+	// if err != nil {
+	// 	fmt.Printf("errored connecting to mongo: %s", err.Error())
+	// 	// panic(err.Error())
+	// } else {
+	// 	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+	// 		fmt.Printf("errored pinging mongo: %s", err.Error())
+	// 		// panic(err)
+	// 	}
+	// }
 
 	msg["body"] = fmt.Sprintf("%s\n Mongo Pinged", msg["body"])
 
