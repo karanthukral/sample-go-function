d9eca5fd16cb50a5c6f077c7d9557f049878984e
Karan Thukral <karanthukral@gmail.com>
2022-03-29 12:58:48 -0400

    use tls

--- packages/default/hello/hello.go
+++ packages/default/hello/hello.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"os"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 func Main(args map[string]interface{}) map[string]interface{} {
@@ -11,18 +18,41 @@ func Main(args map[string]interface{}) map[string]interface{} {
 		name = "stranger"
 	}
 	msg := make(map[string]interface{})
-	msg["body"] = "Hello " + name + "!" + os.Getenv("CA_CERT")
+	msg["body"] = "Hello " + name + "!"
 
-	// client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(os.Getenv("DB_URL")))
-	// if err != nil {
-	// 	fmt.Printf("errored connecting to mongo: %s", err.Error())
-	// 	// panic(err.Error())
-	// } else {
-	// 	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
-	// 		fmt.Printf("errored pinging mongo: %s", err.Error())
-	// 		// panic(err)
-	// 	}
-	// }
+	caCert := os.Getenv("CA_CERT")
+	if caCert != "" {
+		msg["body"] = fmt.Sprintf("%s\n found CA_CERT", msg["body"])
+	}
+
+	opts := options.Client()
+	opts.ApplyURI(os.Getenv("DB_URL"))
+
+	roots := x509.NewCertPool()
+	ok = roots.AppendCertsFromPEM([]byte(caCert))
+	if !ok {
+		panic("cert didn't work")
+	}
+	opts.SetTLSConfig(&tls.Config{
+		RootCAs: roots,
+	})
+
+	client, err := mongo.NewClient(opts)
+	if err != nil {
+		fmt.Printf("client failed: %s", err.Error())
+		// panic(err.Error())
+	} else {
+		ctx := context.Background()
+		err = client.Connect(ctx)
+		if err != nil {
+			fmt.Printf("errored connecting mongo: %s", err.Error())
+		} else {
+			if err := client.Ping(ctx, readpref.Primary()); err != nil {
+				fmt.Printf("errored pinging mongo: %s", err.Error())
+				// panic(err)
+			}
+		}
+	}
 
 	msg["body"] = fmt.Sprintf("%s\n Mongo Pinged", msg["body"])
 
